a234fc6b6aa54fa8baaf551822f2e96dedb2b884
Jordi Baylina <jordi@baylina.cat>
2019-12-24 09:23:42 +0100

    Update test_snarkjs.js

--- test_snarkjs.js
+++ test_snarkjs.js
@@ -3,18 +3,29 @@ const buildBn128 = require('websnark').buildBn128;
 const fs = require('fs');
 const snarkjs = require('snarkjs');
 
-(async function() {
-  const provingKey = snarkjs.unstringifyBigInts(JSON.parse(fs.readFileSync('proving_key.json', 'utf8')));
-  const verifierKey = snarkjs.unstringifyBigInts(JSON.parse(fs.readFileSync('verification_key.json', 'utf8')));
-  const witness = snarkjs.unstringifyBigInts(JSON.parse(fs.readFileSync('witness.json', 'utf8')));
+function copyBuff(src) {
+    var dst = new ArrayBuffer(src.byteLength);
+    new Uint8Array(dst).set(new Uint8Array(src));
+    return dst;
+}
 
-  const {proof, publicSignals} = snarkjs.groth.genProof(provingKey, witness);
-  if (snarkjs.groth.isValid(verifierKey, proof, publicSignals)) {
+(async function() {
+  const bn128 = await buildBn128();
+
+  const signals = fs.readFileSync('witness.bin');
+  const provingKey = fs.readFileSync('proving_key.bin');
+  const proofS = await bn128.groth16GenProof(copyBuff(signals), copyBuff(provingKey));
+
+  console.log(JSON.stringify(proofS, null, 1));
+  const proof = snarkjs.unstringifyBigInts(proofS);
+  const verifierKey = snarkjs.unstringifyBigInts(JSON.parse(fs.readFileSync('verification_key.json', 'utf8')));
+  const pub = snarkjs.unstringifyBigInts(JSON.parse(fs.readFileSync('public.json', 'utf8')));
+
+  if (snarkjs.groth.isValid(verifierKey, proof, pub)) {
     console.log('OK');
   } else {
     console.log('FAILED');
     process.exit(1);
   }
-
-  fs.writeFileSync('public.json', (JSON.stringify(snarkjs.stringifyBigInts(publicSignals))));
+  bn128.terminate();
 })();
